049784ea9bcaee11ade6652fe7e66e2115049dca
Nick Rosbrook <nrosbrook@mail.smcvt.edu>
2017-06-01 18:00:59 -0400

    master: corrected compiler wrapper calls in testutils.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='tyr',
       version='1.1',
-      description='lightweight test and build server',
+      description='lightweight build-and-test server',
       url='',
       author='Nick Rosbrook',
       author_email='nrosbrook@mail.smcvt.edu',
--- tyr/testutils.py
+++ tyr/testutils.py
@@ -8,6 +8,44 @@ from tyr import events
 
 parser = SafeConfigParser()
 
+class compilers(object):
+    """
+    Each method in the compiler class is meant to
+    emulate a different compiler. New methods can
+    be added here to support new languages
+
+    """
+    @staticmethod
+    def gcc(path, inputList, outputList, libsList):
+        # compiler name
+        c = resources.strings.COMPILER_C
+        err = ""
+        # Execute the compiler
+        for i in range(len(inputList)):
+            cmd = c + " -o " + os.path.join(path, outputList[i]) + " " + os.path.join(path, inputList[i])
+            cmd = cmd.split(" ")
+            ret = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            err += ret
+        return err
+
+    @staticmethod
+    def gpp(path, inputList, outputList, libsList):
+        # compiler name
+        c = resources.strings.COMPILER_CPP
+        err = ""
+        # Execute the compiler
+        for i in range(len(inputList)):
+            cmd = c + " -o " + os.path.join(path, outputList[i]) + " " + os.path.join(path, inputList[i])
+            for j in range(len(libsList)):
+                cmd += " --" + libsList[j]
+            cmd = cmd.split(" ")
+            try:
+                ret = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+                err += ret
+            except subprocess.CalledProcessError as cpe:
+                err += cpe.output
+        return err
+
 class controller(object):
 
     def __init__(self, path_srvr):
@@ -27,13 +65,9 @@ class controller(object):
         # Call the compiler
         err = ""
         if language == resources.strings.LANG_C:
-            err = TyrCompiler.gcc(path, inputList, outputList, libsList)
+            err = compilers.gcc(path, inputList, outputList, libsList)
         elif language == resources.strings.LANG_CPP:
-            err = TyrCompiler.gpp(path, inputList, outputList, libsList)
-        elif language == resources.strings.LANG_PYTHON:
-            err = TyrCompiler.python(path, inputList)
-        elif language == resources.strings.LANG_JAVA:
-            err = TyrCompiler.java(path, inputList)
+            err = compilers.gpp(path, inputList, outputList, libsList)
         else:
             print resources.strings.ERR_NO_LANG + language
         return err
